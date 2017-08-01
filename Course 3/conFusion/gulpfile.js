06a0918d44684e8ad65bcbbdf1d022831c738ba5
SemOtaku <semotaku@gmail.com>
2017-08-01 14:32:38 +0300

    Week 3 Assignment: Single Page Applications

--- Course 3/conFusion/gulpfile.js
+++ Course 3/conFusion/gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-    return gulp.src('./app/index.html')
+    return gulp.src('/app/*.html')
         .pipe(usemin({
             css:[minifycss(),rev()],
             js: [ngannotate(),uglify(),rev()]
@@ -75,9 +75,10 @@ gulp.task('browser-sync', ['default'], function () {
 
     browserSync.init(files, {
         server: {
-            baseDir: "app",
+            baseDir: "dist",
             index: "index.html"
-        }
+        },
+        reloadDelay: 1000
     });
     // Watch any files in dist/, reload on change
     gulp.watch(['dist/**']).on('change', browserSync.reload);
