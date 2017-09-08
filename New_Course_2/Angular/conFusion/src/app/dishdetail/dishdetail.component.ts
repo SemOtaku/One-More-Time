d829dcf0ebd67471bb43348ae13e28f25e097679
SemOtaku <semotaku@gmail.com>
2017-09-08 16:04:48 +0300

    Week 3 Assignment
    Angular Forms and Reactive Programming

--- New_Course_2/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ New_Course_2/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Params, ActivatedRoute} from "@angular/router";
 import {Location} from "@angular/common";
 import { Dish } from '../shared/dish';
 import {DishService} from "../services/dish.service";
 import 'rxjs/add/operator/switchMap';
+
+import { FormBuilder, FormGroup, Validators, FormGroupDirective } from '@angular/forms';
+import {Comment} from "../shared/comment";
 
 @Component({
   selector: 'app-dishdetail',
@@ -17,8 +20,30 @@ export class DishdetailComponent implements OnInit {
   prev: number;
   next: number;
 
+  commentForm: FormGroup;
+  comment: Comment;
 
-  constructor(private dishservice: DishService, private location: Location, private route: ActivatedRoute) { }
+  formErrors = {
+    'author': '',
+    'commentText': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required': 'Name is required.',
+      'minlength': 'Name must be at least 2 characters long.'
+    },
+    'commentText': {
+      'required': 'Your commentary is required.'
+    }
+  }
+
+  @ViewChild(FormGroupDirective) commentFormDirective;
+
+
+  constructor(private dishservice: DishService, private location: Location, private route: ActivatedRoute, private cm: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {
     this.dishservice.getDishIds()
@@ -26,6 +51,49 @@ export class DishdetailComponent implements OnInit {
     this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
       .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id)});
+  }
+
+  createForm() {
+    this.commentForm = this.cm.group({
+      author: ['', [Validators.required,Validators.minLength(2)]],
+      rating: 5,
+      commentText: ['', Validators.required]
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) {return;}
+    const form = this.commentForm;
+
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  onSubmit(){
+    this.comment = this.commentForm.value;
+    this.comment.date = new Date().toISOString();
+    this.comment.comment = this.commentForm.value.commentText;
+    console.log(this.comment);
+    this.dish.comments.push(this.comment);
+    this.commentForm.reset({
+      author: '',
+      rating: 5,
+      comment: ''
+    });
+    this.commentFormDirective.resetForm();
   }
 
   setPrevNext(dishId: number) {
