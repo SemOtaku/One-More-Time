c7ec1ec4f61a048dc95bf00fde2dea5ea8f5554a
SemOtaku <semotaku@gmail.com>
2017-09-14 18:33:55 +0300

    Angular HTTP Client: Error Handling

--- New_Course_2/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ New_Course_2/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -23,6 +23,8 @@ export class DishdetailComponent implements OnInit {
   commentForm: FormGroup;
   comment: Comment;
 
+  errMess: string;
+
   formErrors = {
     'author': '',
     'commentText': ''
@@ -55,7 +57,7 @@ export class DishdetailComponent implements OnInit {
       .subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id)});
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id)}, errmess => this.errMess = <any>errmess);
   }
 
   createForm() {
--- New_Course_2/Angular/conFusion/src/app/home/home.component.ts
+++ New_Course_2/Angular/conFusion/src/app/home/home.component.ts
@@ -17,6 +17,8 @@ export class HomeComponent implements OnInit {
   dish: Dish;
   promotion: Promotion;
   featleader: Leader;
+  dishErrMess: string;
+  promErrMess: string;
 
   constructor(private dishservice: DishService,
               private promotionservice: PromotionService,
@@ -25,7 +27,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.dishservice.getFeaturedDish()
-      .subscribe(dish =>  this.dish = dish);
+      .subscribe(dish =>  this.dish = dish, errmess => this.dishErrMess = <any>errmess );
     this.promotionservice.getFeaturedPromotion()
       .subscribe(promotion => this.promotion = promotion);
     this.leaderservice.getFeaturedLeader()
